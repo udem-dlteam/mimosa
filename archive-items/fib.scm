a2ae439f798990eb4c6b550aebb8a536f32eeb4d
Marc Feeley <feeley@iro.umontreal.ca>
2019-08-10 07:15:29 -0400

    New gsc.exe with mkdir and x86-32 backend

---
+++ archive-items/fib.scm
@@ -0,0 +1,13 @@
+(declare
+  (standard-bindings)
+  (extended-bindings)
+  (not safe)
+  (block))
+
+(define (fib n)
+  (if (fx< n 2)
+      n
+      (fx+ (fib (fx- n 1))
+           (fib (fx- n 2)))))
+
+;;(pretty-print (time (fib 40)))
