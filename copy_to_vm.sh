72de682e6e2686a5807b1e4e380b658627b04e2a
SamuelYvon <samuel.yvon@umontreal.ca>
2020-01-15 09:43:03 -0500

    Better script for copying to VM (bootstrap and all)

--- copy_to_vm.sh
+++ copy_to_vm.sh
@@ -1,12 +1,26 @@
-# Copy the content of the build folder into the ubuntu VM
-# This might be useful if you want to rebuild the gambit program for the target
-
+# This script will prepare the environment so Gambit can be built on
+# the ubuntu-6 VM which represents the Mimosa target.
 make clean
-echo "Copying to VM..."
+echo "Compiling Gambit..."
 rm -rf libc/gambit
 cd libc
 git clone https://github.com/gambit/gambit.git
-cd ..
+
+# Prepare gambit
+cd gambit # in libc/gambit
+./configure
+make
+make bootstrap
+make bootclean
+# Use all the available power to speedup things
+make -j
+make dist
+
+cd .. # in libc folder
+# Need to add the archive
+# rm -rf libc/gambit
+
+echo "Copying to VM..."
 tar --exclude='*.img' -czf - . | ssh administrator@localhost -p 10022 "rm -rf mimosa-build; mkdir -p mimosa-build; cd mimosa-build; tar xzf -;";
-rm -rf libc/gambit
+
 echo "Done!"
