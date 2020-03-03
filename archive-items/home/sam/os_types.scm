a86b11eb10fc58d9404f743554abd12b4859671c
Samuel Yvon <samuel.yvon@umontreal.ca>
2020-03-03 18:21:27 -0500

    Merge

--- archive-items/home/sam/os_types.scm
+++ archive-items/home/sam/os_types.scm
@@ -5,8 +5,16 @@
 (define (bcd->binary byte)
  (+ (* 10 (fxarithmetic-shift-right byte 4)) (fxand byte #x0F)))
 
-(define (char-debug-write char)
- (outb char DEBUG-WRITE-PORT))
+
+(define (debug-write obj)
+ (cond ((string? obj)
+        (str-debug-write obj))
+       ((vector? obj)
+        (vector-debug-write obj))
+       ((number? obj)
+        (number-debug-write obj))
+       (else
+        (str-debug-write (string obj)))))
 
 (define (debug-write str)
   (let ((str (if (not (string? str))
@@ -19,3 +27,25 @@
                    (char-debug-write c)))
              (string->list str))
         str))))
+
+(define (str-debug-write str)
+  (let ((str (string-append "[SCM] " str "\n")))
+    (begin
+      (map (lambda (c)
+             (if (char? c)
+                 (char-debug-write c)))
+           (string->list str))
+      str)))
+
+(define (number-debug-write nbr)
+    (str-debug-write (number->string nbr)))
+
+(define (vector-debug-write vect)
+ (let ((l (vector-length vect))
+       (lst-rpr (vector->list vect)))
+  (debug-write (vector-ref vect 0))
+  (debug-write (vector-ref vect 1))
+  (debug-write (vector-ref vect 2))))
+
+(define (char-debug-write char)
+ (outb char DEBUG-WRITE-PORT))
