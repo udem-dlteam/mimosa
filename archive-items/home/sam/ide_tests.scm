d403311596c5306e0802aaa2688fe33778f55235
Samuel Yvon <samuel.yvon@umontreal.ca>
2020-03-04 17:43:50 -0500

    Write test handles

--- archive-items/home/sam/ide_tests.scm
+++ archive-items/home/sam/ide_tests.scm
@@ -18,3 +18,17 @@
          (devices (ide-controller-devices ctrl))
          (device (car devices)))
     (ide-read-sectors device lba 1 cont)))
+
+(define (dsk1)
+  (let* ((ctrl (vector-ref IDE-CTRL-VECT 0))
+         (devices (ide-controller-devices ctrl))
+         (device (car devices)))
+    device))
+
+; (define (ide-write-sectors device lba buffer count)
+(define (ide-write-test)
+  (let ((vect (make-vector 512 #x0))
+        (msg "This is the test"))
+    (for-each (lambda (i)
+        (vector-set! vect i (char->integer (string-ref msg i)))) (iota (string-length msg)))
+    (ide-write-sectors (dsk1) 0 vect 1))) 
