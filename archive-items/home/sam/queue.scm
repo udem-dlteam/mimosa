d01ac987309934fe9a8fce82dcc829f083fc4d31
SamuelYvon <samuel.yvon@umontreal.ca>
2020-03-06 09:01:01 -0500

    Cleaned the queue
    Signed-off-by: SamuelYvon <samuel.yvon@umontreal.ca>

--- archive-items/home/sam/queue.scm
+++ archive-items/home/sam/queue.scm
@@ -1,91 +1,83 @@
 ; The mimosa project
 ; A scheme fixed size queue
+(define-library (queue)
+ (import (gambit))
+ (export get-size make pop peek push empty?)
+ (begin
+   (define (__get-backing-vector q)
+     (vector-ref q 3))
 
-(namespace ("_queue#"
-            make
-            get-size
-            get-lo
-            get-hi
-            empty?
-            push
-            pop
-            ))
+   (define (__set-hi! hi q)
+     (begin
+       (vector-set! q 1 hi)
+       hi))
 
-;;;============================================================================
+   (define (__set-lo! lo q)
+     (begin
+       (vector-set! q 0 lo)
+       lo))
 
-(define (__get-backing-vector q)
-  (vector-ref q 3))
+   (define (__set-at! idx val q)
+     (let ((b (__get-backing-vector q)))
+       (vector-set! b idx val)
+       val))
 
-(define (__set-hi! hi q)
-  (begin
-    (vector-set! q 1 hi)
-    hi))
+   (define (__get-at idx q)
+     (let ((b (__get-backing-vector q)))
+       (vector-ref b idx)))
 
-(define (__set-lo! lo q)
-  (begin
-    (vector-set! q 0 lo)
-    lo))
+   (define (make size)
+     ; A queue contains a lo position, a hi position, a size and the vector itself
+     (vector 0 0 (+ size 1) (make-vector (+ size 1) (list))))
 
-(define (__set-at! idx val q)
-  (let ((b (__get-backing-vector q)))
-    (vector-set! b idx val)
-    val))
+   (define (get-size q)
+     (vector-ref q 2))
 
-(define (__get-at idx q)
-  (let ((b (__get-backing-vector q)))
-    (vector-ref b idx)))
+   (define (get-lo q)
+     (vector-ref q 0))
 
-(define (make size)
-  ; A queue contains a lo position, a hi position, a size and the vector itself
-  (vector 0 0 (+ size 1) (make-vector (+ size 1) (list))))
+   (define (get-hi q)
+     (vector-ref q 1))
 
-(define (get-size q)
-  (vector-ref q 2))
+   (define (empty? q)
+     (let ((lo (get-lo q))
+           (hi (get-hi q)))
+       (= lo hi)))
 
-(define (get-lo q)
-  (vector-ref q 0))
+   ; push an Element into the Queue
+   (define (push element queue)
+     (let* ((e element)
+            (q queue)
+            (hi (get-hi q))
+            (lo (get-lo q))
+            (sz (get-size q))
+            (n-hi (modulo (+ 1 hi) sz)))
+       (if (= n-hi lo)
+           #f 
+           (begin
+             (__set-at! hi element q)
+             (__set-hi! n-hi q)
+             element))))
 
-(define (get-hi q)
-  (vector-ref q 1))
+   (define (pop queue)
+     (let* ((q queue)
+            (hi (get-hi q))
+            (lo (get-lo q))
+            (sz (get-size q)))
+       (if (= lo hi)
+           #f
+           (let ((e (__get-at lo q)))
+             (__set-at! lo (list) q)
+             (__set-lo! (modulo (+ 1 lo) sz) q)
+             e))))
 
-(define (empty? q)
-  (let ((lo (get-lo q))
-        (hi (get-hi q)))
-    (= lo hi)))
-
-; push an Element into the Queue
-(define (push element queue)
-  (let* ((e element)
-         (q queue)
-         (hi (get-hi q))
-         (lo (get-lo q))
-         (sz (get-size q))
-         (n-hi (modulo (+ 1 hi) sz)))
-    (if (= n-hi lo)
-        #f 
-        (begin
-          (__set-at! hi element q)
-          (__set-hi! n-hi q)
-          element))))
-
-(define (pop queue)
-  (let* ((q queue)
-         (hi (get-hi q))
-         (lo (get-lo q))
-         (sz (get-size q)))
-    (if (= lo hi)
-        #f
-        (let ((e (__get-at lo q)))
-          (__set-at! lo (list) q)
-          (__set-lo! (modulo (+ 1 lo) sz) q)
-          e))))
-
-(define (peek queue)
-  (let* ((q queue)
-         (hi (get-hi q))
-         (lo (get-lo q))
-         (sz (get-size q)))
-    (if (= lo hi)
-        #f
-        (let ((e (__get-at lo q)))
-          e))))
+   (define (peek queue)
+     (let* ((q queue)
+            (hi (get-hi q))
+            (lo (get-lo q))
+            (sz (get-size q)))
+       (if (= lo hi)
+           #f
+           (let ((e (__get-at lo q)))
+             e))))
+   ))
