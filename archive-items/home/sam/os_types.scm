7fb44241784ac69fe2e63149ee83b47147dfee15
SamuelYvon <samuel.yvon@umontreal.ca>
2020-03-09 09:35:23 -0400

    Clean
    Signed-off-by: SamuelYvon <samuel.yvon@umontreal.ca>

--- archive-items/home/sam/os_types.scm
+++
@@ -1,4 +0,0 @@
-;; The mimosa project
-
-(define (bcd->binary byte)
- (+ (* 10 (fxarithmetic-shift-right byte 4)) (fxand byte #x0F)))
--- archive-items/home/sam/utils.scm
+++ archive-items/home/sam/utils.scm
@@ -145,5 +145,7 @@
                     (flatten c)
                     (cons c (flatten (cdr ipt))))))))
 
+      (define (bcd->binary byte)
+        (+ (* 10 (fxarithmetic-shift-right byte 4)) (fxand byte #x0F)))
 
       )) 
