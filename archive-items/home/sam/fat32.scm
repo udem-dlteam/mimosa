ce8ef17765f29c32a512c9ab742c4045ea912d39
SamuelYvon <samuel.yvon@umontreal.ca>
2020-03-15 13:55:18 -0400

    Better macro

--- archive-items/home/sam/fat32.scm
+++ archive-items/home/sam/fat32.scm
@@ -28,12 +28,28 @@
 
       (define-macro (define-c-struct name . fields)
                     (let* ((symbol-name (symbol->string name))
-                           (fill-struct
+                           (pack-struct
                              (string->symbol (string-append "pack-" symbol-name)))
+                           (unpack-struct
+                             (string->symbol (string-append "unpack-" symbol-name)))
                            (make-struct
                              (string->symbol (string-append "make-" symbol-name)))
                            (width-struct
                              (string->symbol (string-append symbol-name "-width")))
+                           (field-accessor (lambda (field)
+                                             (string->symbol 
+                                               (string-append
+                                                 symbol-name
+                                                 "-"
+                                                 (symbol->string field)))))
+                           (flatten (lambda (lst)
+                                      (if (pair? lst)
+                                          (fold-right (lambda (e r)
+                                                        (if (pair? e)
+                                                            (append (flatten (car e)) (flatten (cdr e)) r)
+                                                            (cons e r)))
+                                                      (list) lst)
+                                          (list lst))))
                            (vect-idx 0)
                            (field-names (map car fields))
                            (field-width (map cadr fields)))
@@ -41,12 +57,46 @@
                          (define-structure ,name 
                                            ,@field-names)
                          (define ,width-struct (+ ,@field-width))
-                         (define (,fill-struct vec)
+                         (define (,unpack-struct strc vec)
+                           ; fields that are shorter than 4 are packed into an int
+                           ; fields that are longer than 4 are packed directly into a vect
+                           ; fields that have a negative length are forced into a vect
+                           (begin
+                             ,@(let ((offset 0))
+                                (map (lambda (f-index)
+                                       (let ((f (list-ref field-names f-index))
+                                             (w (list-ref field-width f-index)))
+                                         ; copy the vector into the target vector
+                                         (if (or (< w 0) (> w 4))
+                                              `(begin ,@(map 
+                                                          (lambda (i)
+                                                            (set! offset (+ offset 1))
+                                                            `(vector-set!
+                                                               vec
+                                                               ,(- offset 1)
+                                                               (vector-ref (,(field-accessor f) strc) ,i)))
+                                                          (iota (abs w))))
+                                              `(begin ,@(map (lambda (i)
+                                                               (set! offset (+ offset 1))
+                                                               `(vector-set!
+                                                                  vec
+                                                                  ,(- offset 1)
+                                                                  (fxand #xFF (arithmetic-shift
+                                                                           (,(field-accessor f) strc)
+                                                                           ,(- 0 (* i 8)))))
+                                                               )
+                                                             (iota w)))
+                                             ))) (iota (length field-names))))))
+
+                         (define (,pack-struct vec)
                            (,make-struct
                              ;; TODO: map does not guarantee left-to-right calls to function
+                             ; fields that are shorter than 4 are packed into an int
+                             ; fields that are longer than 4 are packed directly into a vect
+                             ; fields that have a negative length are forced into a vect
                              ,@(map (lambda (extract)
                                       (let ((offset vect-idx)
-                                            (next-offset (+ vect-idx extract)))
+                                            (next-offset (+ vect-idx (abs extract))))
                                         (set! vect-idx next-offset)
                                         (if (<= 0 extract 4)
                                             `(+ ,@(map (lambda (i)
